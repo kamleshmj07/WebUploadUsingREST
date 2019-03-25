c74db76306b69f68cacb199d5f4dec28f71f6b83
Kamlesh Mj <kamleshmj07@gmail.com>
2019-03-25 15:09:01 +0530

    Initial Commit

---
+++ WebUploadUtility/Controllers/DocFileController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using System.IO;
+namespace FileUpload.Controllers
+{
+    // Similar article https://www.c-sharpcorner.com/article/upload-large-files-in-web-api-with-a-simple-method/
+    public class DocFileController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    //postedFile.FileName = postedFile.FileName + DateTime.Now.ToString();
+                    var filePath = HttpContext.Current.Server.MapPath("~/Archive/" + postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+                    docfiles.Add(filePath);
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
+        }
+    }
+}
